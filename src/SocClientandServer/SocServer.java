408ecb2a54113dc645758edfca8b7fe00d9ba7e4
Sahil0718 <sahil.bhattarai46@gmail.com>
2025-07-27 22:41:06 +0545

    Completed up to SocServer_TCP

---
+++ src/SocClientandServer/SocServer.java
@@ -0,0 +1,93 @@
+package SocClientandServer;
+
+//import java.net.ServerSocket;
+//import java.net.Socket;
+//import java.io.*;
+//
+//public class SocServer {
+//    public static void main(String[] args) throws Exception {
+//        int port = 9999;
+//
+//        System.out.println("Server has been started!");
+//        ServerSocket ss = new ServerSocket(port);
+
+//        System.out.println("Server is waiting for client request!");
+//
+//        Socket s = ss.accept(); //Waits here
+//        System.out.println("Client Connected!");
+//
+//        //Reads the data from the client
+//        //Wraps it in an InputStreamReader and then a Buffered Reader to read text lines easily
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+//
+//        String message;
+//        while ((message = reader.readLine()) != null) {
+//            System.out.println("Received: " + message);
+//        }
+//        //The loop continues until the client closes the output stream (or socket), causing the readLine() to return Null
+//
+//        s.close();
+//        ss.close();
+//        //Closes the client socket and the server socket, releasing the sources and ending the program
+//        System.out.println("Connection Closed!");
+//    }
+//}
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.*;
+import java.net.*;
+
+public class SocServer {
+    public static void main(String[] args) throws Exception {
+        int port = 9999;
+
+        System.out.println("SERVER HAS STARTED SUCCESSFULLY!");
+        ServerSocket ss = new ServerSocket(port);
+
+        System.out.println("SERVER IS WAITING FOR THE CLIENT REQUEST");
+
+        Socket s = ss.accept(); //waits here
+        System.out.println("Client connected successfully!");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+        String message;
+        while((message = reader.readLine()) != null){
+            System.out.println("THE MESSAGE RECEIVED IS: "+message);
+        }
+        s.close();
+        ss.close();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
